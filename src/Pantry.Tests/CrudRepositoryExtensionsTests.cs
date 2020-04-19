85db51b374eced0be52ad56be228b641945ec3cc
Julien Blin <julien.blin@gmail.com>
2020-04-19 11:00:12 -0400

    Added UpdateLatestAsync extension method.

---
+++ src/Pantry.Tests/CrudRepositoryExtensionsTests.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Moq;
+using Pantry.Exceptions;
+using Pantry.Generators;
+using Pantry.Tests.StandardTestSupport;
+using Xunit;
+
+namespace Pantry.Tests
+{
+    public class CrudRepositoryExtensionsTests
+    {
+        [Fact]
+        public async Task ItShouldApplyUpdate()
+        {
+            var currentEntity = new StandardEntity
+            {
+                Id = await new GuidIdGenerator<StandardEntity>().Generate(null),
+            };
+
+            var updatedEntity = new StandardEntity
+            {
+                Id = currentEntity.Id,
+            };
+
+            var updatedEntityThroughRepo = new StandardEntity
+            {
+                Id = currentEntity.Id,
+            };
+
+            var repo = new Mock<ICrudRepository<StandardEntity>>();
+            repo.Setup(x => x.GetByIdAsync(currentEntity.Id, It.IsAny<CancellationToken>()))
+                .ReturnsAsync(currentEntity);
+
+            repo.Setup(x => x.UpdateAsync(updatedEntity, It.IsAny<CancellationToken>()))
+                .ReturnsAsync(updatedEntityThroughRepo);
+
+            var result = await repo.Object.UpdateLatestAsync(
+                currentEntity.Id,
+                e =>
+                {
+                    e.Should().Be(currentEntity);
+                    return updatedEntity;
+                });
+
+            result.Should().Be(updatedEntityThroughRepo);
+        }
+
+        [Fact]
+        public async Task ItShouldApplyUpdatesRepeateadlyWhenConcurrencyIssue()
+        {
+            var currentEntity = new StandardEntity
+            {
+                Id = await new GuidIdGenerator<StandardEntity>().Generate(null),
+            };
+
+            var updatedEntity = new StandardEntity
+            {
+                Id = currentEntity.Id,
+            };
+
+            var updatedEntityThroughRepo = new StandardEntity
+            {
+                Id = currentEntity.Id,
+            };
+
+            var repo = new Mock<ICrudRepository<StandardEntity>>();
+            repo.Setup(x => x.GetByIdAsync(currentEntity.Id, It.IsAny<CancellationToken>()))
+                .ReturnsAsync(currentEntity);
+
+            repo.SetupSequence(x => x.UpdateAsync(updatedEntity, It.IsAny<CancellationToken>()))
+                .ThrowsAsync(new ConcurrencyException())
+                .ThrowsAsync(new ConcurrencyException())
+                .ReturnsAsync(updatedEntityThroughRepo);
+
+            var result = await repo.Object.UpdateLatestAsync(
+                currentEntity.Id,
+                e =>
+                {
+                    e.Should().Be(currentEntity);
+                    return updatedEntity;
+                });
+
+            result.Should().Be(updatedEntityThroughRepo);
+
+            repo.Verify(x => x.GetByIdAsync(currentEntity.Id, It.IsAny<CancellationToken>()), Times.Exactly(3));
+        }
+
+        [Fact]
+        public async Task ItShouldGiveUpWhenMaximumTimesAchieved()
+        {
+            var currentEntity = new StandardEntity
+            {
+                Id = await new GuidIdGenerator<StandardEntity>().Generate(null),
+            };
+
+            var updatedEntity = new StandardEntity
+            {
+                Id = currentEntity.Id,
+            };
+
+            var updatedEntityThroughRepo = new StandardEntity
+            {
+                Id = currentEntity.Id,
+            };
+
+            var repo = new Mock<ICrudRepository<StandardEntity>>();
+            repo.Setup(x => x.GetByIdAsync(currentEntity.Id, It.IsAny<CancellationToken>()))
+                .ReturnsAsync(currentEntity);
+
+            repo.Setup(x => x.UpdateAsync(updatedEntity, It.IsAny<CancellationToken>()))
+                .ThrowsAsync(new ConcurrencyException());
+
+            Func<Task> act = async () => await repo.Object.UpdateLatestAsync(
+                currentEntity.Id,
+                e =>
+                {
+                    e.Should().Be(currentEntity);
+                    return updatedEntity;
+                },
+                10);
+
+            act.Should().Throw<ConcurrencyException>();
+
+            repo.Verify(x => x.GetByIdAsync(currentEntity.Id, It.IsAny<CancellationToken>()), Times.Exactly(11));
+        }
+    }
+}
---
+++ src/Pantry/CrudRepositoryExtensions.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Pantry.Exceptions;
+
+namespace Pantry
+{
+    /// <summary>
+    /// <see cref="ICrudRepository{TEntity}"/> extension methods.
+    /// </summary>
+    public static class CrudRepositoryExtensions
+    {
+        /// <summary>
+        /// Allows the application of an Update based on the guaranteed lastest version of the entity,
+        /// if the provider supports concurrency control (most do).
+        /// Will retry up to <paramref name="numberOfRetries"/> on concurrency issues.
+        /// </summary>
+        /// <typeparam name="TEntity">The entity type. Must be <see cref="IETaggable"/>.</typeparam>
+        /// <param name="repository">The <see cref="ICrudRepository{TEntity}"/>.</param>
+        /// <param name="id">The entity id.</param>
+        /// <param name="update">
+        /// The update functions that modifies the entity.
+        /// Receives the lastes version and must return the modified version.
+        /// Return null to cancel the update.
+        /// May be called multiple time in a tight loop.
+        /// </param>
+        /// <param name="numberOfRetries">The maximum number of times to retry the Get/Update cycle before giving up.</param>
+        /// <param name="cancellationToken">The <see cref="CancellationToken"/>.</param>
+        /// <returns>The updated entity, or null if it was cancelled inside the <paramref name="update"/> function.</returns>
+        /// <exception cref="NotFoundException">If the entity could not be found.</exception>
+        /// <exception cref="ConcurrencyException">If the <paramref name="numberOfRetries"/> was reached.</exception>
+        public static async Task<TEntity?> UpdateLatestAsync<TEntity>(
+            this ICrudRepository<TEntity> repository,
+            string id,
+            Func<TEntity, TEntity?> update,
+            int numberOfRetries = 10,
+            CancellationToken cancellationToken = default)
+            where TEntity : class, IIdentifiable, IETaggable
+        {
+            if (repository is null)
+            {
+                throw new ArgumentNullException(nameof(repository));
+            }
+
+            if (string.IsNullOrEmpty(id))
+            {
+                throw new ArgumentNullException(nameof(id));
+            }
+
+            while (true)
+            {
+                try
+                {
+                    var entity = await repository.GetByIdAsync(id, cancellationToken: cancellationToken).ConfigureAwait(false);
+                    var updatedEntity = update(entity);
+                    if (updatedEntity is null)
+                    {
+                        return null;
+                    }
+
+                    return await repository.UpdateAsync(updatedEntity, cancellationToken: cancellationToken).ConfigureAwait(false);
+                }
+                catch (ConcurrencyException)
+                {
+                    if (numberOfRetries > 0)
+                    {
+                        --numberOfRetries;
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+            }
+        }
+    }
+}
