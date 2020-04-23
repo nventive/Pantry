e571fc9469d4ea73257e2d4de8107edf3dc7ca0b
Julien Blin <julien.blin@gmail.com>
2020-04-23 13:04:03 -0400

    Fixed decorator registration bug when repository have unknown interfaces.

--- src/Pantry/DependencyInjection/RepositoryBuilderExtensions.cs
+++ src/Pantry/DependencyInjection/RepositoryBuilderExtensions.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Linq;
 using Pantry;
+using Pantry.Decorators;
 using Pantry.DependencyInjection;
 
 namespace Microsoft.Extensions.DependencyInjection
@@ -13,20 +15,22 @@ namespace Microsoft.Extensions.DependencyInjection
         /// Adds a repository decorator.
         /// </summary>
         /// <typeparam name="TEntity">The entity type.</typeparam>
+        /// <typeparam name="TDecorator">The type of decorator.</typeparam>
         /// <param name="builder">The <see cref="IRepositoryBuilder{TEntity}"/>.</param>
         /// <param name="decoratorFactory">The decorator factory.</param>
         /// <returns>The updated <see cref="IRepositoryBuilder{TEntity}"/>.</returns>
-        public static IRepositoryBuilder<TEntity> AddRepositoryDecorator<TEntity>(
+        public static IRepositoryBuilder<TEntity> AddRepositoryDecorator<TEntity, TDecorator>(
             this IRepositoryBuilder<TEntity> builder,
-            Func<object, IServiceProvider, object> decoratorFactory)
+            Func<object, IServiceProvider, TDecorator> decoratorFactory)
             where TEntity : class, IIdentifiable
+            where TDecorator : RepositoryDecorator<TEntity>
         {
             if (builder is null)
             {
                 throw new ArgumentNullException(nameof(builder));
             }
 
-            foreach (var repositoryInterface in builder.RegisteredRepositoryInterfaces)
+            foreach (var repositoryInterface in builder.RegisteredRepositoryInterfaces.Where(x => x.IsAssignableFrom(typeof(TDecorator))))
             {
                 builder.Services.TryDecorate(repositoryInterface, decoratorFactory);
             }
